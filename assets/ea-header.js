fffdeed047a52f423ad00675ab456a4e18cfe553
Emmanuel-A <emmanuel@theorydigital.ca>
2024-04-25 18:10:06 -0400

    Updated megamenu - Hover to reveal

--- assets/ea-header.js
+++ assets/ea-header.js
@@ -1,47 +1,24 @@
 
-class MegaMenu extends HTMLElement {
-    constructor() {
-      super();  
+// class MegaMenu extends HTMLElement {
+//     constructor() {
+//       super();  
 
-      this.querySelectorAll('.first-level-link').forEach(e => e.addEventListener('mouseover', this.hide.bind(this, false)));  
-    //   this.addEventListener('keyup', (event) => {
-    //     if (event.code.toUpperCase() === 'ESCAPE') this.hide();
-    //   });
-    //   if (this.classList.contains('media-modal')) {
-    //     this.addEventListener('pointerup', (event) => {
-    //       if (event.pointerType === 'mouse' && !event.target.closest('deferred-media, product-model')) this.hide();
-    //     });
-    //   } else {
-    //     this.addEventListener('click', (event) => {
-    //       if (event.target === this) this.hide();
-    //     });
-    //   }
-    }
+//       this.querySelectorAll('.first-level-link').forEach(e => e.addEventListener('mouseover', this.hide.bind(this, false)));  
+//     }
   
-    // connectedCallback() {
-    //   if (this.moved) return;
-    //   this.moved = true;
-    //   document.body.appendChild(this);
-    // }
+//     show() {
+//       console.log('Show');
+//       this.querySelector('.header .top-nav-wrapper .first-level-link .second-level-link').classList.remove('show');
+//       document.body.classList.add('overflow-hidden');
+//       window.pauseAllMedia();
+//     }
   
-    // show(opener) {
-    //   this.openedBy = opener;
-    //   const popup = this.querySelector('.template-popup');
-    //   document.body.classList.add('overflow-hidden');
-    //   this.setAttribute('open', '');
-    //   if (popup) popup.loadContent();
-    //   trapFocus(this, this.querySelector('[role="dialog"]'));
-    //   window.pauseAllMedia();
-    // }
-  
-    hide() {
-      console.log('Closed or Hide');
-      document.querySelector
-    //   document.body.classList.remove('overflow-hidden');
-    //   document.body.dispatchEvent(new CustomEvent('modalClosed'));
-    //   this.removeAttribute('open');
-    //   removeTrapFocus(this.openedBy);
-    //   window.pauseAllMedia();
-    }
-  }
-  customElements.define('top-nav-wrapper', MegaMenu);
+//     hide() {
+//       console.log('Hide');
+//       this.querySelector('.header .top-nav-wrapper .first-level-link .second-level-link').classList.add('show');
+//       document.body.classList.remove('overflow-hidden');
+//       window.pauseAllMedia();
+//     }
+    
+//   }
+//   customElements.define('top-nav-wrapper', MegaMenu);
