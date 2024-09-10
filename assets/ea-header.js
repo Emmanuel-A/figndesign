ebe7de87cbd7623d9ef1c60b818d8ca474c37f41
Emmanuel-A <emmanuel.amponsah@uncommonagency.co>
2024-09-10 00:10:11 -0400

    Updates

--- assets/ea-header.js
+++ assets/ea-header.js
@@ -1,24 +1,21 @@
 
-// class MegaMenu extends HTMLElement {
-//     constructor() {
-//       super();  
+class MegaMenu extends HTMLElement {
+    constructor() {
+      super();  
 
-//       this.querySelectorAll('.first-level-link').forEach(e => e.addEventListener('mouseover', this.hide.bind(this, false)));  
-//     }
-  
-//     show() {
-//       console.log('Show');
-//       this.querySelector('.header .top-nav-wrapper .first-level-link .second-level-link').classList.remove('show');
-//       document.body.classList.add('overflow-hidden');
-//       window.pauseAllMedia();
-//     }
-  
-//     hide() {
-//       console.log('Hide');
-//       this.querySelector('.header .top-nav-wrapper .first-level-link .second-level-link').classList.add('show');
-//       document.body.classList.remove('overflow-hidden');
-//       window.pauseAllMedia();
-//     }
-    
-//   }
-//   customElements.define('top-nav-wrapper', MegaMenu);
+      let items = document.querySelector(".header__inline-menu").querySelectorAll("details");
+      items.forEach(item => {
+        item.addEventListener("mouseover", () => {
+          item.setAttribute("open", true);
+          item.querySelector("ul").addEventListener("mouseleave", () => {
+           item.removeAttribute("open");
+          });
+        item.addEventListener("mouseleave", () => {
+          item.removeAttribute("open");
+        });
+      });
+      
+      });         
+    }
+  }
+  customElements.define('top-nav-wrapper', MegaMenu);
